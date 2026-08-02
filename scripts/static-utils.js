b85b7ab362258a55d90cc4f862f979a53cf6af1e
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2026-08-02 09:31:12 -0500

    chore: v2.1.0 (#28)
    * build(deps-dev): bump standard from 17.1.0 to 17.1.2 (#15)
    
    Bumps [standard](https://github.com/standard/standard) from 17.1.0 to 17.1.2.
    - [Release notes](https://github.com/standard/standard/releases)
    - [Changelog](https://github.com/standard/standard/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/standard/standard/compare/v17.1.0...v17.1.2)
    
    ---
    updated-dependencies:
    - dependency-name: standard
      dependency-type: direct:development
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * chore: Upgrades all actions (account for Node.js 24 requirements) (#19)
    
    * chore: Upgrades all actions (account for Node.js 24 requirements)
    * Update lint.yml
    * chore: address lint errors
    * chore: move to env instead of template interp
    
    * feat: Adds 'static_json_path' input to static action (#20)
    
    * feat: adds upload_artifact input to control whether or not the built asset is uploaded (#21)
    
    * chore: Upgrades various actions to latest (mostly Node.js 24 compatibility) (#27)
    
    * chore: Upgrades various actions to latest (mostly Node.js 24 compatibility)
    * chore: lint updates
    * pin actions
    
    * chore: update input value handling
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- scripts/static-utils.js
+++ scripts/static-utils.js
@@ -7,57 +7,59 @@
  */
 
 /**
-* Parse a `static.json` file and return the JSON object.
-* @param {object} env - Environment packages from the Github Action (i.e. core, github, context)
-* @param {object} state - State from the Github Action (i.e. outputs)
-*/
-function parse ({ core }, state = {}) {
-  const fs = require('fs')
-  let config = {}
+ * Parse a `static.json` file and return the JSON object.
+ * @param {object} env - Environment packages from the Github Action (i.e. core, github, context)
+ * @param {object} state - State from the Github Action (i.e. outputs)
+ */
+function parse({ core }, state = {}) {
+  const fs = require("fs");
+  let config = {};
   try {
-    config = JSON.parse(fs.readFileSync('./static.json', 'utf8'))
+    config = JSON.parse(fs.readFileSync("./static.json", "utf8"));
   } catch (e) {
-    core.setFailed(`Unable to parse static.json: ${e.message}`)
+    core.setFailed(`Unable to parse static.json: ${e.message}`);
   }
   // Merge GitHub Action state with the parsed static.json
   config = {
     ...config,
     _static: {
       host: {
-        ...state.host
+        ...state.host,
       },
-      ...config._static
-    }
-  }
+      ...config._static,
+    },
+  };
 
   try {
-    validate(config)
+    validate(config);
   } catch (e) {
-    core.setFailed(`Invalid static.json: ${e.message}`)
+    core.setFailed(`Invalid static.json: ${e.message}`);
   }
-  return config
+  return config;
 }
 
 /**
  * A basic validation function for the `static.json` file.
  */
-function validate (config) {
+function validate(config) {
   if (!config) {
-    throw new Error('No configuration provided.')
+    throw new Error("No configuration provided.");
   }
-  if (!('_static' in config) || !config._static) {
-    throw new Error('`_static` member not found in configuration.')
+  if (!("_static" in config) || !config._static) {
+    throw new Error("`_static` member not found in configuration.");
   }
   if (!config._static?.generator) {
-    throw new Error('A `generator` is required in a `_static` configuration.')
+    throw new Error("A `generator` is required in a `_static` configuration.");
   }
-  if (config._static?.ecosystem && config._static?.ecosystem !== 'npm') {
-    throw new Error('Unknown ecosystem provided. `npm` is currently the only official supported ecosystem.')
+  if (config._static?.ecosystem && config._static?.ecosystem !== "npm") {
+    throw new Error(
+      "Unknown ecosystem provided. `npm` is currently the only official supported ecosystem.",
+    );
   }
-  return true
+  return true;
 }
 
 module.exports = {
   parse,
-  validate
-}
+  validate,
+};
